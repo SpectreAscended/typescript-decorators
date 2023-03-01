46e1229d98942406a13fd0b20b67bc3e57a357c0
Cory Pollard <astandard87@gmail.com>
2023-03-01 12:17:03 -0600

    Added notes on decorator factories

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,60 @@
 
 // Decorators execute when your class is defined, not when it is instantiated.  You don't need to instantiate your class at all and this decorator will still run.
 
-function Logger(constructor: Function) {
-  console.log('Logging...');
-  console.log(constructor);
+// function Logger(constructor: Function) {
+//   console.log('Logging...');
+//   console.log(constructor);
+// }
+
+// @Logger
+// class Person {
+//   name = 'Cory';
+
+//   constructor() {
+//     console.log('Creating person object...');
+//   }
+// }
+
+// const pers = new Person();
+
+// console.log(pers);
+
+// Decorator factories
+
+// We pass a our decorator function into the return statement of the Logger function.  We then accept an argument that will specify which decorator we wish to use.
+
+// This will give use more options to configure what the decorator does.
+
+function Logger(logString: string) {
+  console.log('LOGGER FACTORY');
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+function WithTemplate(template: string, hookId: string) {
+  console.log('TEMPLATE FACTORY');
+
+  return function (constructor: any) {
+    console.log('Rendering template');
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  };
+}
+
+// Decorator factories/ meta programming essentailly are a tool which we expose to other developers which they can use to, for example, conveniently render something on the screen for a givin class. Angular uses decorators a lot.
+
+// You can add more than one decorator.
+// They execute from the bottom up.  In this example @WithTemplate will execute first, and then @Logger will execute second.  However, the factories will run in the order in which they are defined in the script.
+
+// @Logger('LOGGING - PERSON')
+@Logger('LOGGING')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
   name = 'Cory';
 
@@ -23,3 +71,35 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// If you add a decorator to a property the decorate recieves 2 arguments. The first element is the target of the property.  The second argument is the propertyName.
+
+// If we console log the two arguments we will see that target is the prototype of our class and the propertyName will be the ... properties name. --> 'title'
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator');
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - should be positive!');
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
